28f76c04c2e0b65e1853747f8d9bded3061fdcae
Brian Jesse <brian.jesse@gmail.com>
2018-02-21 01:02:24 -0600

    feat(store): add persist
    persist is a localstorage interface for the store

---
+++ localstorage/index.js
@@ -0,0 +1,73 @@
+/* eslint-disable security/detect-object-injection */
+
+const TEST = '_TEST_LOCALSTORAGE_AVAILABLE_';
+let storage = window.localStorage;
+
+try {
+  window.sessionStorage.setItem(TEST, '1');
+  window.sessionStorage.removeItem(TEST);
+} catch (error) {
+  // In-memory storage when localStorage is not available, e.g. iOS private browsing mode.
+  let data = {};
+  storage = {
+    _data: data,
+    setItem: (id, val) => {
+      data[id] = String(val);
+    },
+    getItem: id => {
+      let output = data[id];
+      if (output === undefined) {
+        return null;
+      }
+      return output;
+    },
+    removeItem: id => delete data[id],
+    clear: () => {
+      data = {};
+    },
+  };
+}
+
+/**
+ * LocalStorage wrapper
+ *  - These will be saved in localStorage as the property name you use.
+ * @module store
+ */
+module.exports = {
+  /**
+   * Remove a key from localStorage
+   * @function
+   * @param {string} key - The key of a property to remove from localStorage
+   * @example
+   *   store.remove('myKey');
+   */
+  remove: (...args) => storage.removeItem(...args),
+
+  /**
+   * Clear all of localStorage
+   * @function
+   * @example
+   *   store.clear();
+   */
+  clear: () => storage.clear(),
+
+  /**
+   * Get an item from LocalStorage
+   * @function
+   * @param {string} key - The key of a property to retrieve from LocalStorage
+   * @return {*} - The JSON parsed result retrieved from LocalStorage
+   * @example
+   *   store.get('myKey');
+   */
+  get: key => JSON.parse(storage.getItem(key)),
+
+  /**
+   * Set an item in LocalStorage
+   * @function
+   * @param {string} key - The key of a property to set in LocalStorage
+   * @param {*} value - The value of an object to set LocalStorage (will be converted to JSON)
+   * @example
+   *   store.set('myKey', myVal);
+   */
+  set: (key, value) => storage.setItem(key, JSON.stringify(value)),
+};
--- store/index.js
+++ store/index.js
@@ -1,52 +1,101 @@
 /* eslint-disable security/detect-object-injection */
 let Vue = require('vue');
 let Vuex = require('vuex');
+let localstorage = require('../localstorage');
 
 const state = {};
+const PERSIST = '$persist';
+const RESTORE = '_$restore';
 
 Vue.use(Vuex);
-let store = new Vuex.Store({ state });
-let modules = {};
+let getKey = path => `_store_module_${path}_`;
+let store = new Vuex.Store({
+  state,
 
+  mutations: {
+    [RESTORE](context, path) {
+      Object.assign(context[path], localstorage.get(getKey(path)));
+    },
+  },
+
+  modules: {
+    [PERSIST]: {
+      namespaced: true,
+
+      actions: {
+        restore({ commit }, path) {
+          commit(RESTORE, path, { root: true });
+        },
+
+        set({ rootState }, path) {
+          localstorage.set(getKey(path), rootState[path]);
+        },
+
+        clear() {
+          localstorage.clear();
+        },
+      },
+    },
+  },
+});
+let modules = {};
 let register = (id, obj) => {
   store.registerModule(modules[id], obj);
 };
+let toContext = helpers => Object.entries(helpers).reduce((out, [name, helper]) => {
+  out[name] = (...args) => Object.assign({ $store: store }, helper(...args));
+  return out;
+}, {});
+let { mapActions } = toContext(Vuex.createNamespacedHelpers(PERSIST));
+let $persist = mapActions(['restore', 'set', 'clear']);
+let persisting = [];
 
-module.exports = (id, obj) => {
+module.exports = Object.assign((id, obj) => {
   obj.namespaced = true;
   register(id, obj);
-  let helpers = Vuex.createNamespacedHelpers(modules[id]);
+  let path = modules[id];
+  let helpers = Vuex.createNamespacedHelpers(path);
+  helpers.context = toContext(helpers);
+  if (obj.persist) {
+    persisting.push(path);
+    $persist.restore(path);
+  }
   if (obj.actions && obj.actions.init) {
-    let { mapActions } = helpers;
-    let context = Object.assign({ $store: store }, mapActions(['init']));
+    let context = helpers.context.mapActions(['init']);
     context.init();
   }
   return helpers;
-};
-module.exports.store = store;
+}, {
+  store,
+  PERSIST,
+  $persist,
 
-// store.subscribe((...args) => {
-//   console.log(args);
-// });
+  context(appModule, context) {
+    if (module.hot) {
+      let hot = [];
+      register = (id, obj) => {
+        let path = modules[id];
+        if (hot.includes(id)) {
+          store.hotUpdate({ modules: { [path]: obj } });
+        } else {
+          hot.push(id);
+          store.registerModule(path, obj);
+        }
+      };
+      appModule.hot.accept(context.id, () => {});
+    }
+    context.keys().forEach(key => {
+      modules[context.resolve(key)] = key
+        .replace(/^\.\//, '')
+        .replace(/(\/index)?\.js$/, '');
+    });
+    return store;
+  },
+});
 
-module.exports.context = (appModule, context) => {
-  if (module.hot) {
-    let hot = [];
-    register = (id, obj) => {
-      let path = modules[id];
-      if (hot.includes(id)) {
-        store.hotUpdate({ modules: { [path]: obj } });
-      } else {
-        hot.push(id);
-        store.registerModule(path, obj);
-      }
-    };
-    appModule.hot.accept(context.id, () => {});
+store.subscribe(({ type }) => {
+  let path = type.substring(0, type.lastIndexOf('/'));
+  if (persisting.includes(path)) {
+    $persist.set(path);
   }
-  context.keys().forEach(key => {
-    modules[context.resolve(key)] = key
-      .replace(/^\.\//, '')
-      .replace(/(\/index)?\.js$/, '');
-  });
-  return store;
-};
+});
